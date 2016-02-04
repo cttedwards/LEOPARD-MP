51babf1a19a5dd75a31d955810fcdab63facf0c4
Charles Edwards <charles.edwards@niwa.co.nz>
2016-02-04 15:46:33 +1300

    update to rdays
    further explorations

--- code/rdays.R
+++ code/rdays.R
@@ -40,8 +40,8 @@ hist(sim, prob = TRUE, main = 'constant prob.')
 
 ###########
 # 2. Explore relationship between CHANGING CATCHABILITY
-# and distribution of wating times
-n     <- 2000
+# and distribution of waiting times
+n     <- 200
 theta <- 0.1
 
 ilogit <- function(x) exp(x) / (exp(x) + 1)
@@ -59,32 +59,50 @@ plot(catchability, pp, ylim = c(0,1), xlab = 'catchability', ylab = 'prob. encou
 
 sim <- numeric(1000)
 for (i in 1:length(sim)) sim[i] <- rdays(pp)
-hist(sim, prob = TRUE, main = 'increasing prob.')
+hist(sim, prob = TRUE, main = 'linear increasing prob.')
 
-# concave catchability
-#catchability2 <- 1/(10000 - 9995/99 * (1:100 - 1))
-catchability2 <- seq(0.01,2, length.out = 100) ^ 2
+# constant catchability
+catchability2 <- 0.2
 
-plot(catchability)
-lines(catchability2)
+logitpp <- log(catchability2) + theta * log(n)
 
-logitpp2 <- log(catchability2) + theta * log(n)
+pp2 <- ilogit(logitpp)
 
-pp2 <- ilogit(logitpp2)
+plot(1:length(pp2), pp2, ylim = c(0,1), xlab = 'sequential days', ylab = 'prob. encounter', type = 'p')
 
-plot(1:length(pp2), pp2, ylim = c(0,1), xlab = 'sequential days', ylab = 'prob. encounter', type = 'l')
-
-plot(catchability2, pp2, ylim = c(0,1), xlab = 'catchability', ylab = 'prob. encounter', type = 'l')
+plot(catchability2, pp2, ylim = c(0,1), xlab = 'catchability', ylab = 'prob. encounter', type = 'p')
 
 sim <- numeric(1000)
-for (i in 1:length(sim)) sim[i] <- rdays(pp2)
-hist(sim, prob = TRUE)
+for (i in 1:length(sim)) sim[i] <- rdays(pp)
+hist(sim, prob = TRUE, main = 'constant prob.')
+
+
+# concave catchability
+#catchability3 <- 1/(10000 - 9995/99 * (1:100 - 1))
+catchability3 <- seq(0.01,2, length.out = 100) ^ 2
+
+#plot(catchability)
+#lines(catchability3)
+
+logitpp <- log(catchability3) + theta * log(n)
+
+pp3 <- ilogit(logitpp)
+
+plot(1:length(pp), pp, ylim = c(0,1), xlab = 'sequential days', ylab = 'prob. encounter', type = 'l')
+lines(1:length(pp3), pp3, col = 2)
+
+plot(catchability, pp, ylim = c(0,1), xlab = 'catchability', ylab = 'prob. encounter', type = 'l')
+lines(catchability3, pp3, col = 2)
+
+sim <- numeric(1000)
+for (i in 1:length(sim)) sim[i] <- rdays(pp3)
+hist(sim, prob = TRUE, , main = 'non-linear increasing prob.')
 
 ###########
 # 3. Simulate waiting time data
 
 # probablity of kill per day since hunting began
-prob.encounter <- function(d, beta = c(0.01, 0.02, 2), n = 2000, theta = 0.1) {
+prob.encounter <- function(d, beta = c(0.01, 0.02, 2), n = 200, theta = 0.1) {
     
     ilogit <- function(x) exp(x) / (exp(x) + 1)
     
@@ -116,13 +134,34 @@ for (i in 1:length(sim)) sim[i] <- rdays(pp)
 hist(sim, prob = TRUE, main = 'days')
 
 # or linear increase in catchability 
-pp <- prob.encounter(1:100, beta = c(0.001,0.004,1))[[2]]
+pp <- prob.encounter(1:100, beta = c(0.001,0.004,1), n = 200, theta = 0.1)[[2]]
 
-plot(pp, type = 'l', ylim = c(0, 1), xlab = 'sequential days', ylab = 'prob. encounter')
+plot(pp * 0.3, type = 'l', ylim = c(0, 1), xlab = 'sequential days', ylab = 'prob. encounter')
 
 sim <- numeric(1000)
-for (i in 1:length(sim)) sim[i] <- rdays(pp)
+for (i in 1:length(sim)) {
+    tmp <- c()
+    for (j in 1:20) tmp <- c(tmp, rdays(pp * 0.3))
+    sim[i] <- min(tmp) #rdays(pp * 0.2)
+}
 hist(sim, prob = TRUE, main = 'days')
+
+# or constant catchability 
+pp <- prob.encounter(1:200, beta = c(0.05,0.00,1), n = 200, theta = 0.2)[[2]]
+
+plot(pp * 0.3, type = 'l', ylim = c(0, 1), xlab = 'sequential days', ylab = 'prob. encounter')
+
+sim <- numeric(1000)
+for (i in 1:length(sim)) {
+    tmp <- c()
+    for (j in 1:20) tmp <- c(tmp, rgeom(1, pp * 0.3)) #tmp <- c(tmp, rdays(pp * 0.3))
+    sim[i] <- sample(tmp, 1) #rdays(pp * 0.2)
+}
+hist(sim, prob = TRUE, main = 'days')
+
+# success rate
+1 - mean(is.na(sim))
+
 
 # CHECK LIBRARY
 library(leopard)
@@ -131,16 +170,55 @@ age.structure <- c(0.35,0.2,0.06,0.05,0.04,0.04,0.02,0.1,0.04,0.02,0.01,0.01,0.0
 
 xx <- newleopard(2000 * age.structure)
 
+# approximate negative binomial distribution
 days <- numeric(1000)
 for (i in 1:1000) {
-    xx <- newleopard(rbeta(1,  20, 2) * 2000 * age.structure)
     days[i] <- kill(xx,beta = c(0.0001, 0.0004, 1))$waiting.time
 }
 
+hist(days, main = 'linear increase in catchability')
+
 # geometric distribution
-hist(days)
+days <- numeric(1000)
+for (i in 1:1000) {
+    days[i] <- kill(xx, beta = c(0.1, 0, 1), theta = 0.4)$waiting.time
+}
+
+hist(days, main = 'constant catchability')
 
 
 
+#########################################################################
+# the waiting time will decrease as the number of age classes increases #
+# => need alternative formulation
+# => population age/day matrix using conditional probabilities
 
+# probablity of kill per day since hunting began
+prob.kill <- function(d = 1:100, beta = c(0.1, 0.0, 1), n = 200, theta = 0.4, preference = 0.3) {
+    
+    ilogit <- function(x) exp(x) / (exp(x) + 1)
+    
+    catchability <-  (beta[1] + (d - 1) * beta[2]) ^ beta[3]
+    
+    prob.enc <- ilogit(log(catchability) + theta * log(n))
+    
+    prob.kill <- prob.enc * preference
+    
+    return(prob.kill)
+}
 
+age.structure <- c(0.35,0.2,0.06,0.05,0.04,0.04,0.02,0.1,0.04,0.02,0.01,0.01,0.01,0.05)
+
+n <- 2000 * age.structure
+
+prob.kill.at.age <- matrix(0, 14, 100)
+for (i in 1:14)
+    prob.kill.at.age[i,] <- prob.kill(n = n[i], beta = c(0.001, 0.001, 1), theta = 1)
+
+prob.kill.at.age.at.time <- matrix(0, 14, 100)
+prob.kill.at.age.at.time[,1] <- prob.kill.at.age[,1]
+for (i in 2:100)
+    prob.kill.at.age.at.time[,i] <- prob.kill.at.age[,i] * prod(1 - prob.kill.at.age[,1:(i-1)])
+
+plot(prob.kill.at.age.at.time[1,], type = 'l', ylim = c(0,0.2))
+for (i in 2:14) lines(prob.kill.at.age.at.time[i,], col = i)
