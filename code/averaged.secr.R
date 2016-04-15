aa3cf68ceb436661c4bac82b8ae047f24eac15eb
Ross Tyzack Pitman <rosstyzackpitman@gmail.com>
2016-04-15 14:18:55 +0200

    Updated secr averaged code

--- code/averaged.secr.R
+++ code/averaged.secr.R
@@ -9,6 +9,7 @@ source('utils/saver.r')
 source('utils/loader.r')
 source('utils/reader.r')
 source('utils/writer.r')
+source('utils/pdfr.r')
 
 library(ggplot2)
 library(reshape)
@@ -69,79 +70,18 @@ ave.secr.plot <- ggplot(data=ave.secr, aes(x = as.factor(year), y = estimate)) +
   theme_bw() + theme(strip.background = element_rect(fill="white")) +
   xlab("year") + ylab("Density\n per 100km2") + ylim(0,10)
 
-ave.secr.plot  
+ave.secr.plot 
+pdfr(ave.secr.plot, width = 5, height = 5, name = 'ave.secr.plot')
 
-ave.lambda.plot <- ggplot(data=ave.secr, aes(x = as.factor(year), y = lambda)) + 
+lambda.plot <- ggplot(data=ave.secr, aes(x = as.factor(year), y = lambda)) + 
   geom_line() +
   geom_point(shape=21,size=3,fill="white") +
   theme_bw() + theme(strip.background = element_rect(fill="white")) +
   xlab("year") + ylab("lambda") + ylim(0.5,1.25) 
 
-ave.lambda.plot
+lambda.plot
+pdfr(lambda.plot, width = 5, height = 5, name = 'lambda.plot')
 
 ##################################################################################################
 # End
 ##################################################################################################
-
-
-
-
-# do not run
-
-secr.results
-
-# Welgevonden
-welg.gr.2013_2014 <- (secr.results$Density[6] - secr.results$Density[5]) / secr.results$Density[5] * 100
-welg.gr.2013_2015 <- (secr.results$Density[7] - secr.results$Density[5]) / secr.results$Density[5] * 100 / 2
-
-# Atherstone
-ath.gr.2013_2014 <- (secr.results$Density[12] - secr.results$Density[11]) / secr.results$Density[11] * 100
-ath.gr.2013_2015 <- (secr.results$Density[13] - secr.results$Density[11]) / secr.results$Density[11] * 100 / 2
-
-# Lajuma
-lj.gr.2014_2015 <- (secr.results$Density[23] - secr.results$Density[22]) / secr.results$Density[22] * 100
-
-# Makalali
-mak.gr.2014_2015 <- (secr.results$Density[28] - secr.results$Density[27]) / secr.results$Density[27] * 100
-
-# Timbavati
-tim.gr.2013_2014 <- (secr.results$Density[25] - secr.results$Density[24]) / secr.results$Density[24] * 100
-
-# Venetia
-vlnr.gr.2014_2015 <- (secr.results$Density[15] - secr.results$Density[14]) / secr.results$Density[14] * 100
-
-# Wonderkop
-won.gr.2013_2014 <- (secr.results$Density[9] - secr.results$Density[8]) / secr.results$Density[8] * 100
-won.gr.2013_2015 <- (secr.results$Density[10] - secr.results$Density[8]) / secr.results$Density[8] * 100 / 2
-
-### growth rate
-growth.rate <- data.frame(year=c("2013", "2014", "2015"),
-                          lambda=c(1,NA,NA),
-                          sd=c(0,NA,NA))
-
-growth.rate[2,2] <- 1 + (mean(welg.gr.2013_2014, ath.gr.2013_2014, tim.gr.2013_2014, won.gr.2013_2014) / 100)
-growth.rate[2,3] <- sd(c(-25.4902, -16.98113, -2.545455, -70.58824)) / 100
-
-growth.rate[3,2] <- 1 + (mean(welg.gr.2013_2015, ath.gr.2013_2015, lj.gr.2014_2015, mak.gr.2014_2015, vlnr.gr.2014_2015, won.gr.2013_2015) / 100)
-growth.rate[3,3] <- sd(c(-23.52941, -0.9433962, -34.31373, 49.20635, -9.615385, -7.843137)) / 100
-
-
-secr.ave.Combined <- ggplot(data=ave.secr, aes(x = as.factor(year), y = estimate)) + geom_line() + 
-  geom_errorbar(width=.1, aes(ymin=estimate-sd, ymax=estimate+sd)) +
-  geom_point(shape=21,size=3,fill="white")
-
-secr.ave.Combined = secr.Combined + theme_bw() + theme(strip.background = element_rect(fill="white"))
-secr.ave.Combined = secr.Combined + xlab("") + ylab("Density\n per 100km2")
-secr.ave.Combined = secr.Combined + theme(axis.title.x=element_text(size=20))
-secr.ave.Combined = secr.Combined + theme(axis.title.y=element_text(size=20))
-secr.ave.Combined = secr.Combined + theme(axis.text.x=element_text(size=15))
-secr.ave.Combined = secr.Combined + theme(axis.text.y=element_text(size=15)) + ylim(0,15)
-secr.ave.Combined
-# write to file 
-
-ppi = 150
-tiff("secr.ave.combined.plot.tiff",width=8*ppi,height=5*ppi,res=ppi, compression = "lzw")
-secr.Combined
-dev.off()
-
-
