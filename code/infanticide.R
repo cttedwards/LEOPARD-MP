5963b01a35f005f2cf7fde9d49f38f3799f454bb
Charles Edwards <charles.edwards@niwa.co.nz>
2016-04-20 18:35:15 +0200

    more changes to infanticide

--- code/infanticide.R
+++ code/infanticide.R
@@ -9,35 +9,18 @@ source('utils/pdfr.r')
 nfemales <- sum(c(99,80,73,45,194))
 nmales   <- sum(c(41,24,24,28,95))
 
-# harem size
-harem_size <- 5
-
 # create orthogonal data.frame
 nmales   <- nmales * seq(0, 2, length = 101)
 nfemales <- seq(100, 600, by = 100)
 
 dfr <- expand.grid(nm = nmales, nf = nfemales)
-dfr$nh <- dfr$nf / harem_size
 
-number_encounters <- function(nm, nf) {
+# number of encounters between males and females
+# follows the harmonic mean function
+number_encounters <- function(nmales, nfemales) {
     
-    nmales   <- nm
-    nfemales <- nf
-    
-    # number of harems
-    nharems     <- nfemales / harem_size
-    
-    # number of encounters between males and harems
-    nencounters <- 2 * nmales * nharems / (nmales + nharems)
-    
-    # maximum number of encounters per harem assuming
-    # females mate 3 times per year
-    nencounters_max <- nharems * 3
-    
-    # NB this is obsolete because nencounters approaches nharems * 2 as nmales tends to infinity
-    
-    # truncated at <= nencounters_max
-    nencounters <- vapply(nencounters, function(x) min(x, nencounters_max), numeric(1))
+    # number of encounters between males and females
+    nencounters <- 2 * nmales * nfemales / (nmales + nfemales)
     
     # return
     return(nencounters)
@@ -47,18 +30,15 @@ dfr$ne <- apply(dfr, 1, function(x) number_encounters(x[1], x[2]))
 
 ggplot(dfr) + geom_line(aes(nm, ne, col = as.factor(nf))) + ggtitle('number_encounters()')
 
-probability_encounter <- function(nm, nf) {
-    
-    nmales   <- nm
-    nfemales <- nf
+# probability of at least one encounter per female
+# follows a Poisson probability
+probability_encounter <- function(nmales, nfemales) {
     
     nencounters <- number_encounters(nmales, nfemales)
+
+    nencounters_perfemale <- nencounters / nfemales
     
-    nharems     <- nfemales / harem_size
-    
-    nencounters_perharem <- nencounters / nharems
-    
-    pencounter <- 1 - exp(-nencounters_perharem)
+    pencounter <- 1 - exp(-nencounters_perfemale)
     
     return(pencounter)
 }
@@ -67,26 +47,62 @@ dfr$pe <- apply(dfr, 1, function(x) probability_encounter(x[1], x[2]))
 
 ggplot(dfr) + geom_line(aes(nm, pe, col = as.factor(nf))) + ggtitle('probability_encounter()')
 
-gg <- ggplot(dfr) + geom_line(aes(nm, pe, col = as.factor(nf))) + ggtitle('Probability of male occupancy per harem\n') + labs(x = 'Number of mature males', y = '', col = 'Number of mature \nfemales')
+#gg <- ggplot(dfr) + geom_line(aes(nm, pe, col = as.factor(nf))) + ggtitle('Probability of male occupancy per harem\n') + labs(x = 'Number of mature males', y = '', col = 'Number of mature \nfemales')
+#pdfr(gg, width = 10, name = 'pocc')
 
-pdfr(gg, width = 10, name = 'pocc')
+# cub survivorship is a function of male survival
+# and the probability of encounter
+cub_survivorship <- function(nmales, nfemales, smales) {
+    
+    # equilibrium cub survial
+    scub_star <- 0.3270833
+    
+    # probability of mother encountering a male
+    pencounter <- probability_encounter(nmales, nfemales)
+    
+    # probability of infanticide
+    pinfanticide <- pencounter * (1 - smales)
+    
+    scub <- scub_star * (1 - pinfanticide)
+    
+    return(scub)
+    
+}
 
-cub_survivorship <- function(nm, nf, sm) {
+dfr$sm <- seq(0, 1, length = 101)
+
+#dfr <- data.frame(nm = nmales, nf = nfemales, sm = smales)
+#dfr$nm <- dfr$nm * dfr$sm
+
+dfr$sc <- apply(dfr, 1, function(x) cub_survivorship(x[1], x[2], x[3]))
+
+ggplot(dfr) + geom_line(aes(sm, sc)) + ggtitle('Survivorship')
+
+gg <- ggplot(dfr) + geom_line(aes(sm, ss)) + facet_wrap(~age) + ggtitle('Survivorship') + labs(x = 'Survivorship of mature males', y = '')
+
+pdfr(gg, width = 10, name = 'sinf')
+
+
+birth_rate <- function(nf, nm, sm) {
     
     nmales   <- nm
     nfemales <- nf
     smales   <- sm
     
-    scub_star <- 0.3270833
-    sjuv_star <- 0.7197452
-    
     pencounter <- probability_encounter(nmales, nfemales)
     
-    scub <- scub_star * (1 - pencounter * (1 - smales))
-    sjuv <- sjuv_star * (1 - pencounter * (1 - smales))
+    pinfanticide <- pencounter * (1 - smales)
     
-    return(list(scub = scub, sjuv = sjuv))
+    ni <- nf * pinfanticide
     
+    harem_size  <- (3 * nf + ni) / nf
+    clutch_size <- 2
+    
+    nh <- nf / harem_size
+    
+    cubs <- harem_size * clutch_size * 2 * nm * nh / (nm + nh)
+    
+    return(list(h = harem_size, b = cubs))
 }
 
 nfemales <- sum(c(99,80,73,45,194))
@@ -96,19 +112,22 @@ nmales   <- sum(c(41,24,24,28,95))
 smales   <- seq(0, 1, length = 101)
 
 dfr <- data.frame(nm = nmales, nf = nfemales, sm = smales)
-dfr$nm <- dfr$nm * dfr$sm
 
-dfr <- rbind(data.frame(dfr, age = 'Cub',      ss = apply(dfr, 1, function(x) cub_survivorship(x[1], x[2], x[3])[[1]])),
-             data.frame(dfr, age = 'Juvenile', ss = apply(dfr, 1, function(x) cub_survivorship(x[1], x[2], x[3])[[2]])))
+dfr$sc <- apply(dfr, 1, function(x) cub_survivorship(x[1], x[2], x[3]))
+dfr$hs <- apply(dfr, 1, function(x) birth_rate(x[1], x[2], x[3])[[1]])
+dfr$br <- apply(dfr, 1, function(x) birth_rate(x[1], x[2], x[3])[[2]])
 
-ggplot(dfr) + geom_line(aes(sm, ss)) + facet_wrap(~age) + ggtitle('Survivorship')
+ggplot(dfr) + geom_line(aes(sc, hs)) + ggtitle('Harem size')
+
+ggplot(dfr) + geom_line(aes(sc, br)) + ggtitle('Birth rate')
+
+ggplot(dfr) + geom_line(aes(sm, hs)) + ggtitle('Harem size')
+
+ggplot(dfr) + geom_line(aes(sm, br)) + ggtitle('Birth rate')
+
 
 gg <- ggplot(dfr) + geom_line(aes(sm, ss)) + facet_wrap(~age) + ggtitle('Survivorship') + labs(x = 'Survivorship of mature males', y = '')
 
 pdfr(gg, width = 10, name = 'sinf')
-
-
-
-
 
 
