7d2ebe9627fa887551cef53b26ad82f4e019d881
Ross Tyzack Pitman <rosstyzackpitman@gmail.com>
2016-05-27 15:24:14 +0200

    Add recovery years

---
+++ code/three.years.recovery.R
@@ -0,0 +1,97 @@
+
+if(y == 4) {
+  total.removals <- rep(0, 14) + removals$problem_animal@kills
+}
+
+if(y == 5) {
+  total.removals <- rep(0, 14) + removals$problem_animal@kills
+}
+
+if(y == 6) {
+  total.removals <- rep(0, 14) + removals$problem_animal@kills
+}
+
+if(y == 10) {
+  total.removals <- rep(0, 14) + removals$problem_animal@kills
+}
+
+if(y == 11) {
+  total.removals <- rep(0, 14) + removals$problem_animal@kills
+}
+
+if(y == 12) {
+  total.removals <- rep(0, 14) + removals$problem_animal@kills
+}
+
+if(y == 16) {
+  total.removals <- rep(0, 14) + removals$problem_animal@kills
+}
+
+if(y == 17) {
+  total.removals <- rep(0, 14) + removals$problem_animal@kills
+}
+
+if(y == 18) {
+  total.removals <- rep(0, 14) + removals$problem_animal@kills
+}
+
+if(y == 22) {
+  total.removals <- rep(0, 14) + removals$problem_animal@kills
+}
+
+if(y == 23) {
+  total.removals <- rep(0, 14) + removals$problem_animal@kills
+}
+
+if(y == 24) {
+  total.removals <- rep(0, 14) + removals$problem_animal@kills
+}
+
+if(y == 28) {
+  total.removals <- rep(0, 14) + removals$problem_animal@kills
+}
+
+if(y == 29) {
+  total.removals <- rep(0, 14) + removals$problem_animal@kills
+}
+
+if(y == 30) {
+  total.removals <- rep(0, 14) + removals$problem_animal@kills
+}
+
+if(y == 34) {
+  total.removals <- rep(0, 14) + removals$problem_animal@kills
+}
+
+if(y == 35) {
+  total.removals <- rep(0, 14) + removals$problem_animal@kills
+}
+
+if(y == 36) {
+  total.removals <- rep(0, 14) + removals$problem_animal@kills
+}
+
+if(y == 40) {
+  total.removals <- rep(0, 14) + removals$problem_animal@kills
+}
+
+if(y == 41) {
+  total.removals <- rep(0, 14) + removals$problem_animal@kills
+}
+
+if(y == 42) {
+  total.removals <- rep(0, 14) + removals$problem_animal@kills
+}
+
+if(y == 46) {
+  total.removals <- rep(0, 14) + removals$problem_animal@kills
+}
+
+if(y == 47) {
+  total.removals <- rep(0, 14) + removals$problem_animal@kills
+}
+
+if(y == 48) {
+  total.removals <- rep(0, 14) + removals$problem_animal@kills
+}
+
---
+++ code/two.years.recovery.R
@@ -0,0 +1,101 @@
+
+if(y == 3) {
+  total.removals <- rep(0, 14) + removals$problem_animal@kills
+}
+
+if(y == 4) {
+  total.removals <- rep(0, 14) + removals$problem_animal@kills
+}
+
+if(y == 7) {
+  total.removals <- rep(0, 14) + removals$problem_animal@kills
+}
+
+if(y == 8) {
+  total.removals <- rep(0, 14) + removals$problem_animal@kills
+}
+
+if(y == 11) {
+  total.removals <- rep(0, 14) + removals$problem_animal@kills
+}
+
+if(y == 12) {
+  total.removals <- rep(0, 14) + removals$problem_animal@kills
+}
+
+if(y == 15) {
+  total.removals <- rep(0, 14) + removals$problem_animal@kills
+}
+
+if(y == 16) {
+  total.removals <- rep(0, 14) + removals$problem_animal@kills
+}
+
+if(y == 19) {
+  total.removals <- rep(0, 14) + removals$problem_animal@kills
+}
+
+if(y == 20) {
+  total.removals <- rep(0, 14) + removals$problem_animal@kills
+}
+
+
+
+if(y == 23) {
+  total.removals <- rep(0, 14) + removals$problem_animal@kills
+}
+
+if(y == 24) {
+  total.removals <- rep(0, 14) + removals$problem_animal@kills
+}
+
+if(y == 27) {
+  total.removals <- rep(0, 14) + removals$problem_animal@kills
+}
+
+if(y == 28) {
+  total.removals <- rep(0, 14) + removals$problem_animal@kills
+}
+
+if(y == 31) {
+  total.removals <- rep(0, 14) + removals$problem_animal@kills
+}
+
+if(y == 32) {
+  total.removals <- rep(0, 14) + removals$problem_animal@kills
+}
+
+if(y == 35) {
+  total.removals <- rep(0, 14) + removals$problem_animal@kills
+}
+
+if(y == 36) {
+  total.removals <- rep(0, 14) + removals$problem_animal@kills
+}
+
+if(y == 39) {
+  total.removals <- rep(0, 14) + removals$problem_animal@kills
+}
+
+if(y == 40) {
+  total.removals <- rep(0, 14) + removals$problem_animal@kills
+}
+
+
+
+if(y == 43) {
+  total.removals <- rep(0, 14) + removals$problem_animal@kills
+}
+
+if(y == 44) {
+  total.removals <- rep(0, 14) + removals$problem_animal@kills
+}
+
+if(y == 47) {
+  total.removals <- rep(0, 14) + removals$problem_animal@kills
+}
+
+if(y == 48) {
+  total.removals <- rep(0, 14) + removals$problem_animal@kills
+}
+
